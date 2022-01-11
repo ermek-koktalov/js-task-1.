e3bf9cb6d4dcc4ad7776cf300ccd1dba45fad083
ermek-koktalov <95706888+ermek-koktalov@users.noreply.github.com>
2022-01-11 17:29:54 +0100

    add task5

---
+++ js/task5.js
@@ -0,0 +1,8 @@
+let a = Number(+prompt("введите число a"));
+let b = Number(+prompt("введите число b "));
+alert("Сумма a + b= " + (a + b));
+alert("Разница a - b= " + (a - b));
+alert("Произведение a * b= " + a * b);
+alert("Частное от деления a / b= " + a / b);
+alert("Остаток от деления a % b)= " + (a % b));
+alert("Возведение в степень a ** b= " + a ** b);
